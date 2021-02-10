ecf83486ad8ee66cff9930f3c4c6ca43fb60bbfd
Julianne <jbosko28@gmail.com>
2021-02-10 18:37:39 -0500

    created login/signup folders and components

---
+++ src/Components/SignUp/SignUp.js
@@ -0,0 +1,18 @@
+// Directions 
+// copy all code from login.js and paste below (ps. dont delete these comments)
+// delete all code with in the div tags
+// -- replace Login with SignUp in all places in the code 
+
+// -- directions for importing:
+// -- delete all imports except for react import and login.css
+// -- replace "./login.css" with "./signup.css"
+// paste bootstrap code from website - to import react components:
+//    example one: form -> import Form from "react-bootstrap/Form";
+//    example two: button -> import Button from "react-bootstrap/Button"
+
+// once this is all done - go to index.js to create the route for Signup (further directions are there)
+// once you are happy with your code, to check it paste this the below link in you browser:
+// http://localhost:3000/signup
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
+import Login from './Components/Login/Login';
+
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// copy code from line 18-20 paste code in side div tags directly under line 20
+// on line 21 replace "exact path" with "path" (remove the word exact) and replace "/" with "signup"
+// import Signup componnet 
+// on line 22 replace "Login" with "Signup" - then go back to signup component to work on it
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <Router>
+   <div>
+     <Route exact path="/">
+       <Login />
+     </Route>
+
+   </div>
+ </Router>,
   document.getElementById('root')
 );
 
