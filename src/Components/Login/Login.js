6063cf23b38ebdd80a667508660d6fdee5ad0b7e
Julianne <jbosko28@gmail.com>
2021-02-13 16:32:38 -0500

    created signuptest.js, template.js, edited login to connect server

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,22 +5,23 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import "./login.css";
 
-
 class Login extends React.Component {
-  render() {
+   render() {
     return (
       <div className="Login">
         <Container className="mt-5 ml-auto mr-auto">
-        <h1 className="text-center">
-          Welcome to
-          <span className="text-success"> ShareVerse</span>
-        </h1>
+          <h1 className="text-center">
+            Welcome to
+            <span className="text-success"> ShareVerse</span>
+          </h1>
           <Form className="shadow p-3 mb-5 bg-white rounded">
             <Form.Group controlId="formBasicEmail">
-              <Form.Label>User Name</Form.Label>
-              <Form.Control type="email" placeholder="Enter username or email" />
+              <Form.Label>Username:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username or email"
+              />
             </Form.Group>
-
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
@@ -28,10 +29,10 @@ class Login extends React.Component {
             <Button variant="secondary" type="submit">
               Submit
             </Button>
-
-             <Nav.Link className="text-muted" variant="secondary ">New To ShareVerse?</Nav.Link>
-
-             <Button variant="primary" type="submit">
+            <Nav.Link className="text-muted" variant="secondary ">
+              New To ShareVerse?
+            </Nav.Link>
+            <Button variant="primary" type="submit" >
               Sign Up
             </Button>
           </Form>
---
+++ src/Components/Success/SignupSuccess.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Alert from "react-bootstrap/Alert";
+import Container from "react-bootstrap/Container";
+import "./signupsuccess.css";
+
+class SignupSuccess extends React.Component {
+  render() {
+    return (
+      <div className="SignupSuccess">
+        <Container className="mt-5 ml-auto mr-auto">
+          <Alert variant="success">
+            <Alert.Heading>Congradulations</Alert.Heading>
+            <p>You have successfully created your ShareVerse profile.</p>
+            <hr />
+            <p className="mb-0">
+              Go ahead and login, and join the spread of Gods Word, together we
+              can make a differnce!
+            </p>
+          </Alert>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default SignupSuccess;
