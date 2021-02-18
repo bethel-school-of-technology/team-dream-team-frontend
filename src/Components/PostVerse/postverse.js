1c1e68b2f6d02cb2cf440dee6535c0e153c2442b
Cody1087 <codyjc21087@gmail.com>
2021-02-18 14:43:35 -0800

    added postverse component

--- src/Components/PostVerse/postverse.js
+++ src/Components/PostVerse/postverse.js
@@ -3,14 +3,54 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 import './postverse.css';
-import Form from "react-bootstrap/Form"
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+ 
 
 class PostVerse extends React.Component {
+    state = {
+       selectedFile: null, 
+       imagePreview: 'https://www.ormondbeachmartialarts.com/wp-content/uploads/2017/04/default-image.jpg'
+    }
+    
     imageSelectHandler = event => {
-        console.log(event.target.files[0]);
+        const reader = new FileReader();
+        this.setState({
+            selectedFile: event.target.files[0] 
+            })
+        reader.onload = () => {
+            console.log(reader)
+            if(reader.readyState ===2){
+                console.log(reader.result)
+                this.setState({imagePreview: reader.result})
+            }
+        }
+        reader.readAsDataURL(event.target.files[0])
+        
+    }
+    imageUploadHandler = (e) => {
+        
+        
+        const fd = new FormData();
+        fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
+        axios.post('http://localhost:5000/postverse', fd, {
+            onUploadProgress: progressEvent => {
+                (console.log('Upload Progress: ' + Math.round(progressEvent.loaded /
+                progressEvent.total * 100
+                ) + "%"))
+            }
+        }) 
+        .then(res => {
+            console.log(res);
+
+        })
+        .catch(err => {
+            console.log(err);
+          });
     }
 
     render() {
+        const {imagePreview} = this.state
      return (
        <div className="ReplaceMe">
          <Container className="mt-5 ml-auto mr-auto">
@@ -43,11 +83,22 @@ class PostVerse extends React.Component {
                   name="password"               
                 />
               </div>
+             
             </Form.Group>
-            <div classname="imageupload">
-                <input type="file" onChange={this.imageUploadHandler}/>
+            <div className="imageupload">
+                <input                 
+                type="file" 
+                onChange={this.imageSelectHandler}
+                />
+                
+                <Button onClick={this.imageUploadHandler}>Upload</Button>
+                
             </div>
             </Form>
+            <div className='previewholder'>
+                <img src={imagePreview} id="preview" className="preview"/>
+            </div>
+            
           </Container>
        </div>
      );
