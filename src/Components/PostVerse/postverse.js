1f0198907b3f18c9c7337fa36dbf5b6c1f716831
Cody1087 <codyjc21087@gmail.com>
2021-02-19 19:56:24 -0800

    style postverse

--- src/Components/PostVerse/postverse.js
+++ src/Components/PostVerse/postverse.js
@@ -5,111 +5,6 @@ import Button from "react-bootstrap/Button";
 import "./postverse.css";
 import Form from "react-bootstrap/Form";
 import axios from "axios";
-<<<<<<< HEAD
-
-
-class PostVerse extends React.Component {
-    state = {
-        selectedFile: null,
-        imagePreview: 'https://www.ormondbeachmartialarts.com/wp-content/uploads/2017/04/default-image.jpg'
-    }
-
-    imageSelectHandler = event => {
-        const reader = new FileReader();
-        this.setState({
-            selectedFile: event.target.files[0]
-        })
-        reader.onload = () => {
-            console.log(reader)
-            if (reader.readyState === 2) {
-                console.log(reader.result)
-                this.setState({ imagePreview: reader.result })
-            }
-        }
-        reader.readAsDataURL(event.target.files[0])
-
-    }
-    imageUploadHandler = (e) => {
-
-
-        const fd = new FormData();
-        fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
-        axios.post('http://localhost:5000/postverse', fd, {
-            onUploadProgress: progressEvent => {
-                (console.log('Upload Progress: ' + Math.round(progressEvent.loaded /
-                    progressEvent.total * 100
-                ) + "%"))
-            }
-        })
-            .then(res => {
-                console.log(res);
-
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }
-
-    render() {
-        const { imagePreview } = this.state
-        return (
-            <div className="ReplaceMe">
-                <Container className="mt-5 ml-auto mr-auto">
-                    <h1 className="text-center">
-                        Post to
-             <span className="text-success"> ShareVerse</span>
-                    </h1>
-                    <Form
-                        className="shadow p-3 mb-5 bg-white rounded"
-                        id="post-form"
-                    //     onSubmit={this.handleSubmit.bind(this)}
-                    //     method="POST"
-                    >
-
-                        <Form.Group controlId="formBasicVerse">
-                            <Form.Label><h5>Verse:</h5></Form.Label>
-                            <Form.Control
-                                type="textarea"
-                                height="Auto"
-                                required
-
-                            />
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicImage">
-                            <div >
-                                <Form.Label></Form.Label>
-                                <Form.Control
-                                    className="from-control"
-                                    name="password"
-                                />
-                            </div>
-
-                        </Form.Group>
-                        <Form.Group controlId="prev">
-                            <div className="imageupload">
-                                <input
-                                    type="file"
-                                    onChange={this.imageSelectHandler}
-                                    accept="image/*"
-                                />
-
-                                <Button onClick={this.imageUploadHandler}>Upload</Button>
-                            </div>
-                            <div className='previewholder'>
-                                <img src={imagePreview} id="preview" className="preview" />
-                            </div>
-
-
-                        </Form.Group>
-                    </Form>
-
-
-                </Container>
-            </div>
-        );
-    }
-=======
 
 class PostVerse extends React.Component {
   state = {
@@ -205,7 +100,6 @@ class PostVerse extends React.Component {
       </div>
     );
   }
->>>>>>> f2e10278e91d54472af513b29d0c20088dce665f
 }
 
 export default PostVerse;
