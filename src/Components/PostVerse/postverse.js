a75325f4577f41da4e1014c664bf31c9c4f30998
Julianne <jbosko28@gmail.com>
2021-02-23 05:03:21 -0500

    edited route and loyout of bio/pro_img comp

--- src/Components/PostVerse/postverse.js
+++ src/Components/PostVerse/postverse.js
@@ -19,7 +19,7 @@ class PostVerse extends React.Component {
     if (!window.localStorage.getItem("token")) {
       //redirect to Login
       console.log("redirect to login");
-      this.props.history.push("/login");
+      this.props.history.push("/");
     }
 
     if (window.localStorage.getItem("token")) {
@@ -27,7 +27,7 @@ class PostVerse extends React.Component {
         "Authorization"
       ] = window.localStorage.getItem("token");
       axios
-        .post("http://localhost:5000/login")
+        .post("http://localhost:5000/")
         .then((res) => {
           console.log();
           if (!res.data.status === "Login was successful" && 200) {
